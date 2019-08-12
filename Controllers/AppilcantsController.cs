6974594f1e2f427d685ccd3bfe9c79bf094bdc1c
Monty Sinha <monty.sinha2013@live.co.uk>
2019-08-12 14:09:02 +0100

    Add files via upload

---
+++ Controllers/AppilcantsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VisaSystem;
+
+namespace VisaSystem.Controllers
+{
+    public class AppilcantsController : Controller
+    {
+        private DBVisa db = new DBVisa();
+
+        // GET: Appilcants
+        public ActionResult Index()
+        {
+            var appilcants = db.Appilcants.Include(a => a.City).Include(a => a.Countries_).Include(a => a.VisaStatu).Include(a => a.VisaType);
+            return View(appilcants.ToList());
+        }
+
+        // GET: Appilcants/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appilcant appilcant = db.Appilcants.Find(id);
+            if (appilcant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appilcant);
+        }
+
+        // GET: Appilcants/Create
+        public ActionResult Create()
+        {
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "City");
+            ViewBag.VisaID = new SelectList(db.Countries_, "CountryID", "Country");
+            ViewBag.StatusID = new SelectList(db.VisaStatus, "StatusID", "VisaStatus");
+            ViewBag.VisaID = new SelectList(db.VisaTypes1, "VisaID", "VisaType");
+            return View();
+        }
+
+        // POST: Appilcants/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AppID,Firstname,Surname,DateOfBirth,CityID,CountryID,VisaID,StatusID")] Appilcant appilcant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Appilcants.Add(appilcant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "City", appilcant.CityID);
+            ViewBag.VisaID = new SelectList(db.Countries_, "CountryID", "Country", appilcant.VisaID);
+            ViewBag.StatusID = new SelectList(db.VisaStatus, "StatusID", "VisaStatus", appilcant.StatusID);
+            ViewBag.VisaID = new SelectList(db.VisaTypes1, "VisaID", "VisaType", appilcant.VisaID);
+            return View(appilcant);
+        }
+
+        // GET: Appilcants/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appilcant appilcant = db.Appilcants.Find(id);
+            if (appilcant == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "City", appilcant.CityID);
+            ViewBag.VisaID = new SelectList(db.Countries_, "CountryID", "Country", appilcant.VisaID);
+            ViewBag.StatusID = new SelectList(db.VisaStatus, "StatusID", "VisaStatus", appilcant.StatusID);
+            ViewBag.VisaID = new SelectList(db.VisaTypes1, "VisaID", "VisaType", appilcant.VisaID);
+            return View(appilcant);
+        }
+
+        // POST: Appilcants/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AppID,Firstname,Surname,DateOfBirth,CityID,CountryID,VisaID,StatusID")] Appilcant appilcant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(appilcant).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "City", appilcant.CityID);
+            ViewBag.VisaID = new SelectList(db.Countries_, "CountryID", "Country", appilcant.VisaID);
+            ViewBag.StatusID = new SelectList(db.VisaStatus, "StatusID", "VisaStatus", appilcant.StatusID);
+            ViewBag.VisaID = new SelectList(db.VisaTypes1, "VisaID", "VisaType", appilcant.VisaID);
+            return View(appilcant);
+        }
+
+        // GET: Appilcants/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Appilcant appilcant = db.Appilcants.Find(id);
+            if (appilcant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(appilcant);
+        }
+
+        // POST: Appilcants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Appilcant appilcant = db.Appilcants.Find(id);
+            db.Appilcants.Remove(appilcant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VisaSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult MakeAnAppiclation()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult ViewYourAppilcation()
+        {
+            return View();
+        }
+        public ActionResult IndianTourism()
+        {
+            return View();
+        }
+        
+    }
+}
